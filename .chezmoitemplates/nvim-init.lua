a7b2dd5469538368ed7914beb6734c2c0a0d1128
Marcel Schramm <marceloschr@googlemail.com>
2024-04-01 14:45:00 +0200

    Make fzf-native work

--- .chezmoitemplates/nvim-init.lua
+++ .chezmoitemplates/nvim-init.lua
@@ -211,16 +211,13 @@ require('lazy').setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
+
+      -- C reimplementation of fzf core algorithm. This is built by nvim.
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
         build = 'make',
         cond = function()
-          return vim.fn.executable 'make' == 1
+          return vim.fn.executable 'make' == 1 or vim.fn.executable 'cmake' == 1
         end,
       },
     },
