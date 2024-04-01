e3ccbee6132fd0f5d5d173319ea65493769de978
Marcel Schramm <marceloschr@googlemail.com>
2024-04-01 14:21:30 +0200

    Add hop (jump like in vimiumC)

--- .chezmoitemplates/nvim-init.lua
+++ .chezmoitemplates/nvim-init.lua
@@ -118,6 +118,30 @@ require('lazy').setup({
   -- Basic git integration via 'G' command
   { 'tpope/vim-fugitive' },
 
+  {
+    'smoka7/hop.nvim',
+    version = "*",
+    opts = {},
+    config = function()
+      -- place this in one of your configuration file(s)
+      local hop = require('hop')
+      local directions = require('hop.hint').HintDirection
+      vim.keymap.set('', 'f', function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR })
+      end, { remap = true })
+      vim.keymap.set('', 'F', function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR })
+      end, { remap = true })
+      vim.keymap.set('', 't', function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, hint_offset = -1 })
+      end, { remap = true })
+      vim.keymap.set('', 'T', function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, hint_offset = 1 })
+      end, { remap = true })
+      hop.setup()
+    end
+  },
+
   -- Detect tabstop and shiftwidth automatically
   { 'tpope/vim-sleuth' },
 
