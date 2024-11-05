8ec551b97400da4b715ac18a1f29af4b082c7110
Marcel Schramm <marceloschr@googlemail.com>
2024-11-05 12:45:52 +0100

    Add bootstrap script for windows

--- README.md
+++ README.md
@@ -6,13 +6,20 @@ tools might be configured on both systems.
 On Linux, these will most likely be used alongside
 [my NixOS configuration](https://github.com/bios-Marcel/nixos_config).
 
+For Windows a convience bootstrap script is provided. Downloaded it via
+powershell:
+
+```powershell
+Invoke-WebRequest "https://raw.githubusercontent.com/Bios-Marcel/dotfiles/refs/heads/master/setup.ps1" -OutFile setup.ps1
+```
+
 ## Usage
 
 Install [chezmoi](https://www.chezmoi.io/).
 
 ### Bootstrap a new machine:
 
-```
+```shell
 chezmoi init
 chezmoi cd
 git remote add origin git@github.com:Bios-Marcel/dotfiles
---
+++ setup.ps1
@@ -0,0 +1,59 @@
+# Make sure we can execute scripts.
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# Install package management facilities
+if ($null -eq (Get-Command "scoop" -ErrorAction SilentlyContinue))
+{
+	Write-Host "Installing package manager ..."
+	Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+} else
+{
+	Write-Host "Skipping package manager installation, already installed."
+}
+
+# Required to get dotfiles
+scoop install git
+
+if (!(Test-Path -Path "~/.local/share/chezmoi"))
+{
+	Write-Host "Downloading dotfiles ..."
+	git clone "https://github.com/Bios-Marcel/dotfiles.git" "~/.local/share/chezmoi"
+} else
+{
+	Write-Host "Skipping dotfiles download, already downloaded."
+}
+
+Write-Host "Installing tooling ..."
+
+# Install all dependencies used in config
+scoop bucket add extras
+scoop bucket add java
+scoop bucket add nerd-fonts
+scoop bucket add sysinternals
+
+# Core tooling, some might be installed already
+scoop install 7zip chezmoi gpg
+
+# OS stuff, useful for debugging and co.
+scoop install gsudo process-explorer
+
+# Useful terminal tooling
+scoop install fzf ripgrep
+
+# Default font used in config
+scoop install IntelOneMono-NF
+
+# Alternative terminal and shell
+scoop install wezterm nu
+
+# Tooling used for IDE
+scoop install neovim python nodejs openjdk23 gradle maven gcc cmake
+
+# Java dev env
+scoop install idea visualvm jmc tortoisesvn openjdk21 openjdk22
+
+# MISC
+scoop install spotify sharex presenterm
+
+# vi: ft=powershell
+
