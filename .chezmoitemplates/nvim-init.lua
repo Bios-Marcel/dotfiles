be90f9aff047c146edc246a762acbb8c84579133
Marcel Schramm <marceloschr@googlemail.com>
2024-11-19 10:26:25 +0100

    Additional completion shortcut, since Ctrl+Space is broken on win

--- .chezmoitemplates/nvim-init.lua
+++ .chezmoitemplates/nvim-init.lua
@@ -677,7 +677,9 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
+    -- Broken on windows, C-l as alternative
+    ['<C-Space>'] = cmp.mapping.complete({}),
+    ['<C-l>'] = cmp.mapping.complete({}),
     ['<Enter>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
